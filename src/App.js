9f096244887b0cc37f01758d587eb5132bd544e8
춤추는망고 <ensia96@gmail.com>
2020-12-20 11:44:29 +0900

    i forgot when i finished...

--- src/App.js
+++ src/App.js
@@ -1,26 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
+import Movie from "./Movie";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    console.log(movies);
+    this.setState({ movies, isLoading: false });
+  };
+  componentDidMount() {
+    this.getMovies();
+    /*
+    setTimeout(() => {
+      this.setState({ isLoading: false });
+    }, 6000);
+    */
+  }
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">"Loading..."</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+/*
+
+  state = {
+    count: 0,
+  };
+  plus = () => {
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    this.setState((current) => ({
+      count: current.count - 1,
+    }));
+  };
+  componentDidMount() {
+    console.log("mounted");
+  }
+  componentDidUpdate() {
+    console.log("updated");
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
+
+const foodILike = [
+  { id: 1, name: "haha", img: "hoho", rating: 3 },
+  { id: 2, name: "haha1", img: "hoho3" },
+  { id: 3, name: "haha2", img: "hoho2", rating: 3 },
+  { id: 4, name: "haha3", img: "hoho1" },
+];
+
+function Food({ name, img, rating }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is how i laugh! {name}</h1>
+      <p>funny level : {rating} / 5</p>
+      <h2>{img}</h2>
     </div>
   );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+};  
+
+function App() {
+  return (
+    <div>
+      {foodILike.map((food) => (
+        <Food
+          key={food.id}
+          name={food.name}
+          img={food.img}
+          rating={food.rating}
+        />
+      ))}
+    </div>
+  );
+}
+*/
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById("root")); // 컴포넌트 : HTML 을 반환하는 함수 | <App /> 과 같은 모양으로 사용 | JS 와 HTML 사이의 조합을 JSX 라고 부름
